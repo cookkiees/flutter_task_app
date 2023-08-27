41f15098b38c654099e605cfaf3ff47446231b49
Aldo Johanda <johandaaldo00@gmail.com>
2023-08-27 11:31:33 +0700

    Add Home Screen

--- lib/app/modules/home/home_screen.dart
+++ lib/app/modules/home/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -19,34 +19,226 @@ class HomeScreen extends GetView<HomeController> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(24.0),
+          padding: const EdgeInsets.symmetric(vertical: 24.0),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'My Montly Task',
-                        style: MyText.defaultStyle(fontSize: 16),
-                      ),
-                      Text(
-                        '0 Task for this mounth',
-                        style: MyText.subtitleStyle(),
-                      ),
-                    ],
-                  ),
-                  Icon(
-                    Icons.search,
-                    size: 24.0,
-                    color: Colors.grey[400],
-                  ),
-                ],
-              )
+              _buildHeader(),
+              const SizedBox(height: 16),
+              _buildMontlyTasks(),
+              const SizedBox(height: 16),
+              _buildHeaderUpcomingPlans(),
+              ...List.generate(3, (index) {
+                return _buildUpcomingPlans();
+              }),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Padding _buildUpcomingPlans() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 24),
+      child: Container(
+        width: double.infinity,
+        height: 110,
+        margin: const EdgeInsets.symmetric(horizontal: 24),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey[300]!),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Flexible(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 22,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: MyColors.blue.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: const EdgeInsets.only(top: 14, bottom: 14, right: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'WORKING',
+                          style: MyText.subtitleStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Icon(
+                          Icons.more_horiz,
+                          size: 18,
+                          color: Colors.grey[400],
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Meeting for new firm web app',
+                      style: MyText.titleStyle(),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.calendar_month,
+                          size: 14,
+                          color: Colors.grey[400],
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          '24 April',
+                          style: MyText.defaultStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding _buildHeaderUpcomingPlans() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Upcoming Plans',
+            style: MyText.defaultStyle(fontSize: 16),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Text(
+              'See all',
+              style: MyText.titleStyle(color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SizedBox _buildMontlyTasks() {
+    return SizedBox(
+      height: 210,
+      child: CarouselSlider.builder(
+        itemCount: 3,
+        options: CarouselOptions(
+          height: 180,
+          autoPlay: false,
+          enlargeCenterPage: true,
+          viewportFraction: 0.8,
+          autoPlayInterval: const Duration(seconds: 2),
+          autoPlayAnimationDuration: const Duration(milliseconds: 800),
+          autoPlayCurve: Curves.fastOutSlowIn,
+        ),
+        itemBuilder: (BuildContext context, int index, int realIndex) {
+          return _buildContentItemBuilder();
+        },
+      ),
+    );
+  }
+
+  Builder _buildContentItemBuilder() {
+    return Builder(
+      builder: (BuildContext context) {
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                MyColors.blue.withOpacity(0.4),
+                MyColors.blue.withOpacity(0.2),
+                MyColors.blue.withOpacity(0.2),
+                MyColors.blue.withOpacity(0.4),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(
+              color: MyColors.blue.withOpacity(1),
+              width: 0.5,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: MyColors.blue.withOpacity(0.3),
+                blurRadius: 5,
+                spreadRadius: 2,
+                offset: const Offset(0, 8),
+              ),
+              BoxShadow(
+                color: MyColors.blue.withOpacity(0.5),
+                blurRadius: 5,
+                offset: const Offset(0, 0),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: SizedBox(
+        height: 40,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'My Montly Task',
+                  style: MyText.defaultStyle(fontSize: 16),
+                ),
+                Text(
+                  '0 Task for this mounth',
+                  style: MyText.subtitleStyle(),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                VerticalDivider(
+                  color: Colors.grey[300],
+                  indent: 4,
+                  endIndent: 4,
+                ),
+                Icon(
+                  Icons.search,
+                  size: 24.0,
+                  color: Colors.grey[400],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -57,6 +249,8 @@ class HomeScreen extends GetView<HomeController> {
       if (controller.selectedIndex.value == 0) {
         return AppBar(
           backgroundColor: Colors.white,
+          surfaceTintColor: Colors.white,
+          elevation: 0,
           title: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
             child: Row(
